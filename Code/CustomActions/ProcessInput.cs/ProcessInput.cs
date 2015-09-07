6d85b40f8f56df52619cdb508c23228d377a1488
InvertGames <invertgamestudios@gmail.com>
2015-09-07 18:17:21 -0400

    Demo Stuff

---
+++ Code/CustomActions/ProcessInput.cs/ProcessInput.cs
@@ -0,0 +1,14 @@
+namespace uFrameECSDemo {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using uFrame.ECS;
+    
+    
+    public partial class ProcessInput {
+        
+        public override void Execute() {
+        }
+    }
+}
